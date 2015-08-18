0a08210977f2a491e962c70329d60006319edeea
ChuangTseu <gan-ning@hotmail.fr>
2015-08-18 18:59:05 +0200

    Support $ in identifiers

--- Match.h
+++ Match.h
@@ -79,14 +79,14 @@ static const size_t LUT_charColAdvance[256] =
 
 static const bool LUT_validIdFirstChar[256] =
 {
-#define S1(i) (BETWEEN_INCL(i, 'a', 'z') || BETWEEN_INCL(i, 'A', 'Z') || (i == '_'))
+#define S1(i) (BETWEEN_INCL(i, 'a', 'z') || BETWEEN_INCL(i, 'A', 'Z') || (i == '_') || (i == '$'))
 	S256(0)
 #undef S1
 };
 
 static const bool LUT_validIdBodyChar[256] =
 {
-#define S1(i) (BETWEEN_INCL(i, 'a', 'z') || BETWEEN_INCL(i, 'A', 'Z') || BETWEEN_INCL(i, '0', '9') || (i == '_'))
+#define S1(i) (BETWEEN_INCL(i, 'a', 'z') || BETWEEN_INCL(i, 'A', 'Z') || BETWEEN_INCL(i, '0', '9') || (i == '_') || (i == '$'))
 	S256(0)
 #undef S1
 };
